e3865a535bd4edae133c1e6f61f049fdbc254565
DarthKidd <dommbomb@gmailcom>
2018-09-10 22:37:28 +1200

    Updated login page so button links to landing page

--- app/src/main/java/fck2068/example/loginpage/LoginActivity.java
+++ app/src/main/java/fck2068/example/loginpage/LoginActivity.java
@@ -1,13 +1,30 @@
 package fck2068.example.loginpage;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginBtn = (Button) findViewById(R.id.loginButton);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLandingPage();
+            }
+        });
+    }
+
+    public void openLandingPage() {
+        Intent intent = new Intent(this, LandingPageActivity.class);
+        startActivity(intent);
     }
 }
