b7116254d3580976f2760f1570b1016f7c6f5885
DarthKidd <42362465+DarthKidd@users.noreply.github.com>
2018-09-11 17:58:31 +1200

    Add register page / activity

--- app/src/main/java/fck2068/example/loginpage/LoginActivity.java
+++ app/src/main/java/fck2068/example/loginpage/LoginActivity.java
@@ -8,23 +8,37 @@ import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
     private Button loginBtn;
+    private Button createAccBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        loginBtn = (Button) findViewById(R.id.loginButton);
+        loginBtn = findViewById(R.id.loginButton);
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openLandingPage();
             }
         });
+
+        createAccBtn = findViewById(R.id.createAccButton);
+        createAccBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRegisterPage();
+            }
+        });
+    }
+
+    public void openRegisterPage() {
+        Intent regIntent = new Intent(this, RegisterActivity.class);
+        startActivity(regIntent);
     }
 
     public void openLandingPage() {
-        Intent intent = new Intent(this, LandingPageActivity.class);
-        startActivity(intent);
+        Intent loginIntent = new Intent(this, LandingPageActivity.class);
+        startActivity(loginIntent);
     }
 }
